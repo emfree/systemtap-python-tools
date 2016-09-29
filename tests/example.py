2bb850f4ce70cb9175c2dbeb0752755b0faa9bb7
Eben Freeman <freemaneben@gmail.com>
2016-09-29 15:15:11 -0700

    Add py-callgraph script: print pure Python callgraph.
    Should rename this though, so that `scripts/callgraph` becomes what
    `scripts/py-callgraph` is now. Also should document use cases.

--- tests/example.py
+++ tests/example.py
@@ -1,5 +1,16 @@
-def func():
+def callee_a():
     pass
 
+def callee_b():
+    callee_c()
+
+def callee_c():
+    pass
+
+def caller():
+   callee_a()
+   callee_b()
+   callee_a()
+
 while True:
-    func()
+    caller()
